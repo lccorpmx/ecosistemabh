5403bd1b355699e21de4da282e7d564478605cf8
lccorpmx <lcccorpmx@gmail.com>
2023-09-14 13:24:34 -0600

    add pwa to app web

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true, images:{domains:["zhhxicwoypemcvzpqfgl.supabase.co"]}
 }
 
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+ dest: 'public', 
+ register: true,
+ skipWaiting: true,
+ runtimeCaching
+})
+
+module.exports = withPWA({
+ // other congigs
+ reactStrictMode: false
+})
+
 module.exports = nextConfig
