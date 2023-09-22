03725892d1f4f09cc8e7fd0ed65bd72eb02697de
lccorpmx <lcccorpmx@gmail.com>
2023-09-22 13:45:32 -0600

    add carga

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,42 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaFileDownload } from 'react-icons/fa';
 
 export default function DownloadButton() {
-    const downloadPDFs = async () => {
-        try {
-          // Reemplaza estas URLs con las URL directas a tus archivos PDF en GitHub.
-          const pdf1URL = 'https://raw.githubusercontent.com/lccorpmx/MBH/main/MenuBeeHigh.pdf'  + `?t=${Date.now()}`;
-          
-          // Descarga el primer PDF
-          const response1 = await fetch(pdf1URL);
-          const blob1 = await response1.blob();
-          const url1 = window.URL.createObjectURL(blob1);
-        
-          // Crea enlaces de descarga para los PDFs
-          const pdf1Link = document.createElement('a');
-          pdf1Link.href = url1;
-          pdf1Link.download = 'MenuBeeHigh.pdf';
-          document.body.appendChild(pdf1Link);
-          pdf1Link.click();
-          document.body.removeChild(pdf1Link);
-    
-        } catch (error) {
-          console.error('Error al descargar los archivos PDF', error);
-        }
-      };
-    
-      return ( 
-      <div className='flex items-center gap-4 pt-4'>
-        <div>
-        <FaFileDownload
-        onClick={downloadPDFs}
-        className="text-2xl text-white animate-pulse transition duration-5000">
-        </FaFileDownload>
-        </div>
+  const [downloadMessage, setDownloadMessage] = useState('Menu BeeHigh');
 
-        <div>
-            <p onClick={downloadPDFs} className='text-lg text-yellow-400'>Menu BeeHigh</p>
-        </div>
-        </div>
-      );
+  const downloadPDFs = async () => {
+    setDownloadMessage('Descargando Menu...'); // Cambiar el mensaje antes de la descarga
+
+    try {
+      // Reemplaza estas URLs con las URL directas a tus archivos PDF en GitHub.
+      const pdf1URL = 'https://raw.githubusercontent.com/lccorpmx/MBH/main/MenuBeeHigh.pdf' + `?t=${Date.now()}`;
+
+      // Descarga el primer PDF
+      const response1 = await fetch(pdf1URL);
+      const blob1 = await response1.blob();
+      const url1 = window.URL.createObjectURL(blob1);
+
+      // Crea enlaces de descarga para los PDFs
+      const pdf1Link = document.createElement('a');
+      pdf1Link.href = url1;
+      pdf1Link.download = 'MenuBeeHigh.pdf';
+      document.body.appendChild(pdf1Link);
+      pdf1Link.click();
+      document.body.removeChild(pdf1Link);
+
+      setDownloadMessage('Menu Descargado!'); // Restaurar el mensaje después de la descarga
+    } catch (error) {
+      console.error('Error al descargar los archivos PDF', error);
+      setDownloadMessage('Error al descargar PDF');
+    }
+  };
+
+  return (
+    <div className='flex items-center gap-4 pt-4'>
+      <div>
+        <FaFileDownload
+          onClick={downloadPDFs}
+          className="text-2xl text-white animate-pulse transition duration-5000"
+        />
+      </div>
+
+      <div>
+        <p
+          onClick={downloadPDFs}
+          className={`text-lg text-yellow-400 hover:text-white ${
+            downloadMessage === 'Descargando Menu...' ? 'animate-pulse transition duration-5000' : ''
+          }`}
+        >
+          {downloadMessage}
+        </p>
+      </div>
+    </div>
+  );
 }
--- components/DownloadButtonCDMX.tsx
+++ components/DownloadButtonCDMX.tsx
@@ -1,42 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaFileDownload } from 'react-icons/fa';
 
 export default function DownloadButtonCDMX() {
-    const downloadPDFs = async () => {
-        try {
-          // Reemplaza estas URLs con las URL directas a tus archivos PDF en GitHub.
+  const [downloadMessage, setDownloadMessage] = useState('Menu BeeHighCDMX');
 
-          const pdf2URL = 'https://raw.githubusercontent.com/lccorpmx/MBH/main/MenuBeeHighCDMX.pdf'  + `?t=${Date.now()}`;
-        
-          // Descarga el segundo PDF
-          const response2 = await fetch(pdf2URL);
-          const blob2 = await response2.blob();
-          const url2 = window.URL.createObjectURL(blob2);
-    
-    
-          const pdf2Link = document.createElement('a');
-          pdf2Link.href = url2;
-          pdf2Link.download = 'MenuBeeHighCDMX.pdf';
-          document.body.appendChild(pdf2Link);
-          pdf2Link.click();
-          document.body.removeChild(pdf2Link);
-        } catch (error) {
-          console.error('Error al descargar los archivos PDF', error);
-        }
-      };
-    
-      return (
-        <div className='flex items-center gap-4 pt-4'>
-        <div>
+  const downloadPDFs = async () => {
+    setDownloadMessage('Descargando Menu...'); // Cambiar el mensaje antes de la descarga
+
+    try {
+      // Reemplaza estas URLs con las URL directas a tus archivos PDF en GitHub.
+      const pdf2URL = 'https://raw.githubusercontent.com/lccorpmx/MBH/main/MenuBeeHighCDMX.pdf' + `?t=${Date.now()}`;
+
+      // Descarga el segundo PDF
+      const response2 = await fetch(pdf2URL);
+      const blob2 = await response2.blob();
+      const url2 = window.URL.createObjectURL(blob2);
+
+      // Crea enlaces de descarga para los PDFs
+      const pdf2Link = document.createElement('a');
+      pdf2Link.href = url2;
+      pdf2Link.download = 'MenuBeeHighCDMX.pdf';
+      document.body.appendChild(pdf2Link);
+      pdf2Link.click();
+      document.body.removeChild(pdf2Link);
+
+      setDownloadMessage('Menu Descargado!'); // Restaurar el mensaje después de la descarga
+    } catch (error) {
+      console.error('Error al descargar los archivos PDF', error);
+      setDownloadMessage('Error al descargar PDF');
+    }
+  };
+
+  return (
+    <div className='flex items-center gap-4 pt-4'>
+      <div>
         <FaFileDownload
-        onClick={downloadPDFs}
-        className="text-2xl text-white animate-pulse transition duration-5000">
-        </FaFileDownload>
-        </div>
+          onClick={downloadPDFs}
+          className="text-2xl text-white animate-pulse transition duration-5000"
+        />
+      </div>
 
-        <div>
-            <p onClick={downloadPDFs} className='text-lg text-yellow-400'>Menu BeeHigh CDMX</p>
-        </div>
-        </div>
-      );
+      <div>
+        <p
+          onClick={downloadPDFs}
+          className={`text-lg text-yellow-400 hover:text-white ${
+            downloadMessage === 'Descargando Menu...' ? 'animate-pulse transition duration-5000' : ''
+          }`}
+        >
+          {downloadMessage}
+        </p>
+      </div>
+    </div>
+  );
 }
