3f18964234e05c9ac44deea1ef4fa2ce04fac378
Arturo Zilli Rios <111665236+lccorpmx@users.noreply.github.com>
2023-12-11 10:45:44 -0600

    Update home.tsx

--- pages/home.tsx
+++ pages/home.tsx
@@ -148,7 +148,7 @@ export default function Home( {data}:{data:any}) {
          </div>
          </Link>
 
-         <Link href="https://www.instagram.com/bee_highmexbackp/">
+         <Link href="https://www.instagram.com/beehigh_mexico/">
          <div className='flex flex-col items-center'>
             
             <AiOutlineInstagram className='text-pink-600 text-4xl'></AiOutlineInstagram>
