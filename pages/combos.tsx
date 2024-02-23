b5425cc7e6910352f95da35be9d024392d37f7ac
lccorpmx <lcccorpmx@gmail.com>
2024-02-23 00:10:43 -0600

    change slides

--- pages/combos.tsx
+++ pages/combos.tsx
@@ -10,16 +10,51 @@ import { supabase } from './../lib/supabaseClient';
 import { Zoom } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Combos({data}:{data:any}) {
   return (
-    <div>
-        <Header></Header>
-        <div className='relative max-w-md mx-auto'>
-          <SlideShow data={data} title='Combos' title2='BeeHigh'></SlideShow>
-        </div>
+        <div>
+    <Header></Header>
+    <div className='bg-black relative max-w-md mx-auto pl-14 pr-14 pt-8'>
+      <div className='flex justify-center pb-2'>
+        <Link href="/home">
+        <span className='text-yellow-400 text-xl'><span className='text-white'>Combos</span> BeeHigh</span>
+        </Link>
+      </div>
+    <Carousel>
+    <CarouselContent>
+      {data && data.map((imagen: any, index:number) =>
+          <CarouselItem key={index}><Image src={imagen.urlimage} className="w-full" height={1080} width={1080} alt="Logo" /></CarouselItem>
+      )}
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+    </Carousel>
+
+    <div className="bg-green-800 rounded-full mt-2 w-12">
+          <Link href="https://wa.me/5620388381">
+            <div className="flex justify-center items-center p-2">
+              <div>
+              <Image
+                className='rounded-full'
+                src={'/wa.png'}
+                alt='Mi imagen'
+                width={30}
+                height={30}
+              />
+              </div>
+            </div>
+          </Link>
+          </div>
     </div>
+</div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,13 @@ import BotonPersonalizado from '../components/BotonPersonalizado'
 import Popup from "../components/popup"
 import { supabase } from './../lib/supabaseClient';
 import SlideShow from '../components/slideShow'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,8 +28,22 @@ export default function Index( {data}:{data:any}) {
         <meta name="description" content="BeeHigh" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <div className='bg-black relative max-w-md mx-auto'>
-      <Popup data={data}></Popup>
+      <div className='bg-black relative max-w-md mx-auto p-12'>
+        <div className='flex justify-center pb-2'>
+          <Link href="/home">
+          <span className='text-yellow-400 text-xl'><span className='text-white'>Ir a </span>Inicio</span>
+          </Link>
+        </div>
+      <Carousel>
+  <CarouselContent>
+    {data && data.map((imagen, index) =>
+        <CarouselItem key={index}><Image src={imagen.linkimage} className="w-full" height={1080} width={1080} alt="Logo" /></CarouselItem>
+    )}
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
+
       </div>
     </div>
   )
